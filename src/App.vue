f22c4e4dd95450f7e5fdb0f16fae5129491e1b10
Pavel Buta <pavel.buta77@gmail.com>
2024-08-24 20:00:41 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <h1 class="text-3xl text-blue-700">Aplicatie</h1>
+</template>
+
+<style scoped></style>
