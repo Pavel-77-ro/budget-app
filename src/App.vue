49286d59930c127e31c0289e2d5ca3706adbbf5b
Pavel Buta <pavel.buta77@gmail.com>
2024-08-30 12:57:36 +0200

    commit test

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
-  <h1 class="text-3xl text-blue-700">Aplicatie</h1>
+  <h1 class="text-3xl text-blue-700 underline">Aplicatie</h1>
 </template>
 
 <style scoped></style>
